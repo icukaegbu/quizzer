145bfb3b06820ba8e4b02cf4bc7e8214269f0bec
ikedi <icukaegbu@gmail.com>
2014-09-30 11:02:42 +0100

    Added Tests to the fixtures file

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,1 +1,27 @@
-fixtures.js
+if (Tests.find().count() === 0) {
+
+    Tests.insert({
+      title: 'Meteor',
+      description: 'http://meteor.com',
+      submitted: now - 10 * 3600 * 1000,
+      duration: 10,
+      status: 'completed'
+    });
+    Tests.insert({
+      title: 'The Meteor Book',
+      description: 'http://themeteorbook.com',
+      submitted: now - 12 * 3600 * 1000,
+      duration: 50,
+      status: 'in-progress'
+    });
+
+    for (var i = 10; i >= 0; i--) {
+      Tests.insert({
+        title: 'Test post #' + i,
+        description: 'http://me.com',
+        submitted: now - 12 * 3600 * 1000,
+        duration: 40,
+        status: 'pending'
+      });
+    };
+}
