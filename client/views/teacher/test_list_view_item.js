e04fabbd36c410d218a25a54cf0ba6cdcace07e3
ikedi <icukaegbu@gmail.com>
2014-09-30 12:42:42 +0100

    Changed badge colorson TestListViewItem to reflect Test Status

--- client/views/teacher/test_list_view_item.js
+++ client/views/teacher/test_list_view_item.js
@@ -1,9 +1,22 @@
 Template.testListViewItem.helpers({
 	badgeColor: function (e) {
-		var badgeColors = ['primary', 'info', 'danger', 'warning', 'success'];
-		var idx = Math.round(Math.random() * (badgeColors.length - 1));
-		// console.log(idx);
+		// var badgeColors = ['primary', 'info', 'danger', 'warning', 'success'];
+		// var idx = Math.round(Math.random() * (badgeColors.length - 1));
+		// // console.log(idx);
 
-		return badgeColors[idx];
+		// return badgeColors[idx];
+
+		if (this.status === 'pending'){
+			return 'primary';
+		}
+		else if (this.status === 'completed'){
+			return 'success';
+		}
+		else if (this.status === 'in-progress'){
+			return 'danger';
+		}
+		else {
+			return 'warning';
+		}
 	}
 });
