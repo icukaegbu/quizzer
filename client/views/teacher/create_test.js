b6f808bbfe971ec9e1344de880dbac520a6e24af
ikedi <icukaegbu@gmail.com>
2014-09-30 12:37:39 +0100

    Display notification on saving tests

--- client/views/teacher/create_test.js
+++ client/views/teacher/create_test.js
@@ -2,28 +2,46 @@ Template.createTest.events({
 	'submit form': function (e) {
 		e.preventDefault();
 
+		var isSave = false;
+		var test = null;
+		var id = null;
+
 		var title = $(e.target).find("[name=title]").val();
 		var duration = $(e.target).find("[name=duration]").val();
 		var description = $(e.target).find("[name=description]").val();
 
 		//if any field is empty, dont save
+
+		if ( title && duration ){
+			//create an instance of the Test and save
+			test = {
+				title: title,
+				duration: duration,
+				description: description,
+				created: new Date().getTime(),
+				status: 'pending',  //every test starts off ith a pending status
+				bgColor: ''
+			}
+
+			isSave = true;
+
+			id = Tests.insert(test);
+
+			//display notification
+      		Notifications.info('Save Successful', 'Successfully added Test: ');
+
+      		//clear textfiels
+      		$(e.target).find("[name=title]").val('');
+			$(e.target).find("[name=duration]").val('');
+			$(e.target).find("[name=description]").val('');
+		}
+		else {
+			//display error notification
+			Notifications.error('Save Unsuccessful', 'Could not save Test: Title and/or Duration is blank');
+		}
 		
 
-		//create an instance of the Test and save
-		var test = {
-			title: title,
-			duration: duration,
-			description: description,
-			created: new Date().getTime(),
-			status: 'pending',  //every test starts off ith a pending status
-			bgColor: ''
-		}
 
-		var id = Tests.insert(test);
-
-		$(e.target).find("[name=title]").val('');
-		$(e.target).find("[name=duration]").val('');
-		$(e.target).find("[name=description]").val('');
 	},
 
 	'click .cancel': function(e) {
