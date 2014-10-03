673d352e6911912fb7d8ceccbb3c690aaff2fb87
ikedi <icukaegbu@gmail.com>
2014-10-04 00:00:13 +0100

    Change icon and background color when test question is answered

--- client/views/student/test/take_test_item.js
+++ client/views/student/test/take_test_item.js
@@ -1,3 +1,22 @@
+Template.takeTestItem.events({
+	'change .radio': function (e, template) {
+		var t = template;
+
+		//if it has not been clicked before, set the class else leave it
+		if ( !(t.$('div.answered-question').hasClass('alert alert-danger')) ) {
+			//change background and icon
+			// t.$('#'+this.question_num).addClass('alert alert-danger');			
+			// t.$('dd').addClass('alert alert-danger');
+			t.$('div.answered-question').addClass('alert alert-danger');
+			t.$('#toggle'+this.question_num).removeClass('glyphicon glyphicon-bookmark');
+			t.$('#toggle'+this.question_num).addClass('glyphicon glyphicon-ok');
+
+			//save the question so that the answer is pushed to the server screen
+		}
+	}
+});
+
+
 Template.takeTestItem.helpers({
 	getPosition: function (e) {
 
