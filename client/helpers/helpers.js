56c3eebe8b5d56c953790ee318f7e85d95385b4f
ikedi <icukaegbu@gmail.com>
2014-10-05 18:32:30 +0100

    Restrict fields retrieved in getClass/getCategory to only Title

--- client/helpers/helpers.js
+++ client/helpers/helpers.js
@@ -6,9 +6,9 @@
 // })
 
 Template.registerHelper('getCategory', function(id){
-	return Categories.findOne({_id: id}).title;
+	return Categories.findOne({_id: id}, {fields: {title: 1}}).title;
 })
 
 Template.registerHelper('getClass', function(id){
-	return Classes.findOne({_id: id}).title;
+	return Classes.findOne({_id: id}, {fields: {title: 1}}).title;
 })
