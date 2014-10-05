93b8099c85b61773b2a123b9a2a86dd6452cc986
ikedi <icukaegbu@gmail.com>
2014-10-05 18:17:03 +0100

    Created general helper class

---
+++ client/helpers/helpers.js
@@ -0,0 +1,14 @@
+// Template.registerHelper('getCategory', function(){
+// 	return Categories.findOne({_id: this.category}).title;
+// })
+// Template.registerHelper('getClass', function(){
+// 	return Classes.findOne({_id: this.aclass}).title;
+// })
+
+Template.registerHelper('getCategory', function(id){
+	return Categories.findOne({_id: id}).title;
+})
+
+Template.registerHelper('getClass', function(id){
+	return Classes.findOne({_id: id}).title;
+})
