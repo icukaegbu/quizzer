24569e3afee225b97f31cedf3d188e88e67fec5b
ikedi <icukaegbu@gmail.com>
2014-10-05 18:19:43 +0100

    Moved CreateTest's getClass and getCategory to helper class

--- client/views/teacher/test/test_details_view_item.js
+++ client/views/teacher/test/test_details_view_item.js
@@ -1,17 +1,4 @@
 Template.testDetailsViewItem.helpers({
-	getCategory: function (e) {
-		// console.log(e);
-		
-		var t = Categories.find({_id: this.category}).title;
-		// console.log(t);
-
-		return t;
-	},
-
-	getClass: function(e){
-		return Classes.find({_id: this.aclass}).title;
-	},
-
 	getStatusColor: function(){
 		if (this.status === 'pending'){
 			return 'warning';
