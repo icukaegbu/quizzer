7c90a2f9019f03b3881435f745c81eff152ce648
ikedi <icukaegbu@gmail.com>
2014-10-14 13:23:27 +0100

    Create TestResult for each Student in class

--- client/views/teacher/test/administerTest.js
+++ client/views/teacher/test/administerTest.js
@@ -1,36 +1,58 @@
 Template.administerTest.helpers({
-	registerStudents: function (classId) {
+	registerStudents: function () {
+		return TestResults.find({test_id: this._id})
 		//preserve the reference to the test
-		var test = this;
+		// var test = this;
+		// var studentResults = [];
+
+		// //change test status to pending
+		// // this.status = 'in-progress';
+		// Tests.update(this._id, {$set: {status: 'in-progress'}})
+		// // Tests.findOne({_id: this._id}).status = 'in-progress';
+		// console.log(this);
 		
-		//retrieve all students
-		//for each student, create an instance of TestResult
-		//passing in student_id, test_id and array of questions
-		var students = Students.find({aclass: classId});
+		// //retrieve all students
+		// //for each student, create an instance of TestResult
+		// //passing in student_id, test_id and array of questions
+		// var students = Students.find({aclass: classId}, {fields: {_id: 1, name: 1}});
 
-		var testResult = null;
+		// var testResult = null;
 
-		students.forEach(function (student) {
-			testResult = {
-				student_id: student._id,
-				test_id: test._id,
-				test_results: test.questions,
-				created: new Date().getTime()
-			}
+		// console.log(students.count());
 
-			// console.log(testResult)
+		// students.forEach(function (student) {
+		// 	testResult = {
+		// 		test_id: test._id,
+		// 		test_title: test.title,
+		// 		student_id: student._id,
+		// 		student_name: student.name,
+		// 		test_results: test.questions,
+		// 		created: new Date().getTime()
+		// 	}
+		// 	console.log('Inserting');
 
-			// test_result = {
-			// 	// student_id: '',
-			// 	// test_id: ,
-			// 	question_id: this._id,
-			// 	question_num: this.question_num,
-			// 	selected_option: e.target.id,
-			//  	passed: e.target.id === this.answer,
-			// 	submitted: new Date().getTime()
-			// }
-		});
+		// 	var tr = TestResults.insert(testResult);
 
-		return students;
+		// 	// if (tr) {
+		// 	// 	testResult._id = tr;
+		// 	// 	studentResults.push(testResult);
+		// 	// }
+
+		// 	// console.log(testResult)
+
+		// 	// test_result = {
+		// 	// 	// student_id: '',
+		// 	// 	// test_id: ,
+		// 	// 	question_id: this._id,
+		// 	// 	question_num: this.question_num,
+		// 	// 	selected_option: e.target.id,
+		// 	//  	passed: e.target.id === this.answer,
+		// 	// 	submitted: new Date().getTime()
+		// 	// }
+		// });
+
+		//return students;
+		//return studentResults;
+		//return TestResults.find({test_id: this._id})
 	}
 });
--- client/views/teacher/test/test_details_view_item.js
+++ client/views/teacher/test/test_details_view_item.js
@@ -36,17 +36,66 @@ Template.testDetailsViewItem.events({
 			Notifications.error('Delete Unsuccessful', 'Delete failed');
 		}
 	}
-	// ,
+	,
 
-	// //event to handle starting a Test
-	// 'click .glyphicon-play': function(e){
-	// 	e.preventDefault();
+	//event to handle starting a Test
+	'click .glyphicon-play': function(e){
+		//e.preventDefault();
 
-	// 	//create an instance of TestResult for all students
+		//create an instance of TestResult for all students
 
-	// 	//start the timer for the Test
+		//start the timer for the Test
 
-	// 	//Route to the Administer-Test Screen
+		//Route to the Administer-Test Screen
 
-	// }
+
+
+		//preserve the reference to the test
+		var test = this;
+
+		//change test status to pending
+		// this.status = 'in-progress';
+		Tests.update(this._id, {$set: {status: 'in-progress'}})
+		
+		//retrieve all students
+		//for each student, create an instance of TestResult
+		//passing in student_id, test_id and array of questions
+		var students = Students.find({aclass: this.aclass}, {fields: {_id: 1, name: 1}});
+
+		var testResult = null;
+
+		console.log(students.count());
+
+		students.forEach(function (student) {
+			testResult = {
+				test_id: test._id,
+				test_title: test.title,
+				student_id: student._id,
+				student_name: student.name,
+				test_results: test.questions,
+				created: new Date().getTime()
+			}
+			console.log('Inserting');
+
+			var tr = TestResults.insert(testResult);
+
+			// if (tr) {
+			// 	testResult._id = tr;
+			// 	studentResults.push(testResult);
+			// }
+
+			// console.log(testResult)
+
+			// test_result = {
+			// 	// student_id: '',
+			// 	// test_id: ,
+			// 	question_id: this._id,
+			// 	question_num: this.question_num,
+			// 	selected_option: e.target.id,
+			//  	passed: e.target.id === this.answer,
+			// 	submitted: new Date().getTime()
+			// }
+		});
+
+	}
 });
