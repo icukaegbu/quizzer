4a06eed08b9a018267d1729f808fdce739c749f4
ikedi <icukaegbu@gmail.com>
2014-10-15 14:57:00 +0100

    Display Student Name in Dashboard Title when Clicked

--- client/views/student/student_dashboard.js
+++ client/views/student/student_dashboard.js
@@ -1,20 +1,36 @@
 Template.studentDashboard.events({
 	'click .list-group-item': function (e) {
 		//return a list of all pending tests for this student
-		console.log(e.currentTarget);
-		console.log(this);
+		// console.log(e.currentTarget);
+		// console.log(this);
 
 		//store the class and student id in Session, use to retrieve data
+		Session.set('studentClassId', this.aclass);
+		Session.set('studentName', this.name);
+		console.log(this);
+		console.log(Session.get('studentClassId'));
 
-		studentTests = Tests.find({aclass: this.aclass, status: 'pending'});
-		console.log(studentTests.count());
+		// studentTests = Tests.find({aclass: this.aclass, status: 'pending'});
+		// console.log(studentTests.count());
 
-		return studentTests;
+		// return studentTests;
 	}
 });
 
 Template.studentDashboard.helpers({
 	studentTests: function () {
 		//if Session variables for class exist, filter Tests by it, else return all Tests
+		//Tracker.autorun();
+
+		if (Session.get('studentClassId')){
+			return Tests.find({aclass: this.aclass, status: 'pending'});
+		}
+		else {
+			return Tests.find({status: 'pending'});
+		}
+	},
+
+	getStudentName: function(){
+		return Session.get('studentName');
 	}
 });
